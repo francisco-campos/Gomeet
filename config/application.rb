f6946036c7269b3ad6498221ff743cec908fade5
francisco-campos <franciscojavier.c.c@gmail.com>
2016-10-03 17:19:33 -0300

    avance

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,15 @@ module Gomeet
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    #Configuration layout for authetication users
+    config.to_prepare do
+        Devise::SessionsController.layout "lightbox"
+        Devise::RegistrationsController.layout "lightbox"
+        Devise::ConfirmationsController.layout "lightbox"
+        Devise::UnlocksController.layout "lightbox"
+        Devise::PasswordsController.layout "lightbox"
+    end
+
   end
 end
